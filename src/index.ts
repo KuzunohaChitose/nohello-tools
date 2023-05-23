18fe181227e196f632592384b2076cade7b1fe50
Chitose <1595290233@qq.com>
2023-05-23 17:13:06 +0800

    Fixed takeExc

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,7 @@ import * as reactHooks from "./react-hooks";
 import * as vueHooks from "./vue-hooks";
 import * as arrays from "./arrays";
 import * as functions from "./functions";
+import * as objects from "./objects";
+import * as fp_ts from "./fp-ts";
 
-export { curry, reactHooks, vueHooks, arrays, functions };
+export { curry, reactHooks, vueHooks, arrays, functions, objects, fp_ts };
